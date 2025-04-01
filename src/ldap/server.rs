25b0446e9e29386b398e9f57817dbafc36b878d5
Giacomo Longo <gabibbo97@gmail.com>
2025-04-01 19:32:07 +0200

    Improve logging

--- src/ldap/server.rs
+++ src/ldap/server.rs
@@ -8,7 +8,7 @@ use crate::ldap::{attribute::LDAPAttribute, dn::LDAPDN};
 
 use super::{database::LDAPReadOnlyInMemoryDatabase, entry::LDAPEntry};
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub enum BindStatus {
     #[default] Anonymous,
     Bound(Arc<LDAPEntry>),
@@ -16,6 +16,14 @@ pub enum BindStatus {
 impl BindStatus {
     pub fn unbind(&mut self) {
         *self = Self::Anonymous;
+    }
+}
+impl std::fmt::Debug for BindStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Anonymous => write!(f, "anonymous"),
+            Self::Bound(arg0) => write!(f, "bound({})", arg0.dn),
+        }
     }
 }
 
@@ -56,27 +64,46 @@ where
         Ok(())
     }
 
-    #[tracing::instrument(skip(self))]
+    const fn get_ldap_operation_name(op: &ldap3_proto::proto::LdapOp) -> &'static str {
+        match op {
+            ldap3_proto::proto::LdapOp::BindRequest(..) => "bind",
+            ldap3_proto::proto::LdapOp::UnbindRequest => "unbind",
+            ldap3_proto::proto::LdapOp::SearchRequest(..) => "search",
+            _ => "unsupported",
+        }
+    }
+
+    #[tracing::instrument(
+        skip_all,
+        fields(
+            addr = self.addr.to_string(),
+            bind_status = ?self.bind_status,
+            id = &msg.msgid,
+            kind = Self::get_ldap_operation_name(&msg.op),
+            dn
+        )
+    )]
     async fn handle_message(&mut self, msg: ldap3_proto::LdapMsg) -> anyhow::Result<()>
     {
-        tracing::debug!("Handling");
+        let request_kind_name = Self::get_ldap_operation_name(&msg.op);
         match msg.op {
             //
             // Bind
             //
             ldap3_proto::proto::LdapOp::BindRequest(request) => {
                 // metrics
-                counter!("femtoldap_requests_total", "kind" => "bind").increment(1);
+                counter!("femtoldap_requests_total", "kind" => request_kind_name).increment(1);
 
                 // grab dn out of the request
                 let dn = LDAPDN::from_str(&request.dn)?;
+                tracing::Span::current().record("dn", dn.to_string());
 
                 // grab password out of the request
                 let password = match request.cred {
                     ldap3_proto::proto::LdapBindCred::Simple(password) => password,
                     ldap3_proto::proto::LdapBindCred::SASL(_) => {
                         self.io.send(ldap3_proto::LdapMsg { msgid: msg.msgid, op: ldap3_proto::proto::LdapOp::BindResponse(ldap3_proto::proto::LdapBindResponse::new_invalidcredentials(&request.dn, "SASL bind not supported")), ctrl: Vec::new() }).await?;
-                        tracing::event!(Level::INFO, address = ?self.addr, dn = dn.to_string(), "Failed login: SASL bind not supported");
+                        tracing::event!(Level::INFO, "Failed bind: SASL not supported");
                         return Ok(())
                     },
                 };
@@ -84,21 +111,21 @@ where
                 // grab entry
                 if let Some(bound_entry) = self.database.do_bind(&dn, password) {
                     // bind successful
-                    tracing::event!(Level::INFO, address = ?self.addr, dn = dn.to_string(), "Login successful");
+                    tracing::event!(Level::INFO, "Bind successful");
                     self.io.send(ldap3_proto::LdapMsg { msgid: msg.msgid, op: ldap3_proto::proto::LdapOp::BindResponse(ldap3_proto::proto::LdapBindResponse::new_success("")), ctrl: Vec::new() }).await?;
                     self.bind_status = BindStatus::Bound(bound_entry);
                     counter!("femtoldap_successful_binds_total").increment(1);
                 } else {
                     // bind failed
-                    tracing::event!(Level::INFO, address = ?self.addr, dn = dn.to_string(), "Login failed");
+                    tracing::event!(Level::INFO, "Bind failed");
                     self.io.send(ldap3_proto::LdapMsg { msgid: msg.msgid, op: ldap3_proto::proto::LdapOp::BindResponse(ldap3_proto::proto::LdapBindResponse::new_invalidcredentials(&request.dn, "Bind failed")), ctrl: Vec::new() }).await?;
                     counter!("femtoldap_failed_binds_total").increment(1);
                 }
             },
             ldap3_proto::proto::LdapOp::UnbindRequest => {
                 // metrics
-                counter!("femtoldap_requests_total", "kind" => "unbind").increment(1);
-
+                counter!("femtoldap_requests_total", "kind" => request_kind_name).increment(1);
+                tracing::event!(Level::INFO, "Unbound");
                 self.bind_status.unbind();
             },
             //
@@ -106,10 +133,11 @@ where
             //
             ldap3_proto::proto::LdapOp::SearchRequest(request) => {
                 // metrics
-                counter!("femtoldap_requests_total", "kind" => "search").increment(1);
+                counter!("femtoldap_requests_total", "kind" => request_kind_name).increment(1);
 
                 // grab dn out of the request
                 let dn = LDAPDN::from_str(&request.base)?;
+                tracing::Span::current().record("dn", dn.to_string());
 
                 // check access to base DN
                 let can_access = dn.is_empty() || match &self.bind_status {
@@ -189,6 +217,7 @@ where
                     }
 
                     // search success
+                    tracing::event!(Level::INFO, "Searched");
                     self.io.send(ldap3_proto::LdapMsg { msgid: msg.msgid, op: ldap3_proto::proto::LdapOp::SearchResultDone(ldap3_proto::proto::LdapResult {
                         code: ldap3_proto::LdapResultCode::Success,
                         matcheddn: request.base,
@@ -202,7 +231,7 @@ where
             //
             operation => {
                 // metrics
-                counter!("femtoldap_requests_total", "kind" => "unsupported").increment(1);
+                counter!("femtoldap_requests_total", "kind" => request_kind_name).increment(1);
 
                 tracing::warn!(?operation, "Unsupported LDAP operation");
             }
